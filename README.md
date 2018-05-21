e6bcd9565caf8b0c0a68227d5fee81fff89a9362
sumandeepb <sumandeep.banerjee@capillarytech.com>
2018-05-21 06:20:40 +0530

    Updated Readme. Coded the main game loop.

--- README.md
+++ README.md
@@ -10,10 +10,22 @@ Copyright (C) 2018 Sumandeep Banerjee, sumandeep.banerjee@gmail.com
 https://www.linkedin.com/in/sumandeep-banerjee-1436a17/
 
 ## Checking for Graphics Processor
+The following command will tell you about the graphics processor in your system.
+$ sudo lshw -C display
 
 ## Checking for OpenGL Support Version
+The following command shows version information of all OpenGL components installed on your system.
+$ glxinfo | grep 'OpenGL\|version'
 
 ## Installing OpenGL Development Pre-requisites
+$ sudo apt-get update
+$ sudo apt-get install build-essential
+$ sudo apt-get install g++ cmake
+$ sudo apt-get install freeglut3 freeglut3-dev binutils-gold libglew-dev mesa-common-dev libglew1.5-dev libglm-dev
 
-## Installing SFML on Ubuntu
+## Installing SFML(Simple and Fast Multimedia Library) on Ubuntu
+$ sudo apt-get install libsfml-dev
 
+###For more information visit https://www.sfml-dev.org/tutorials/2.5/start-linux.php
+
+Also, to link sfml library into your code use 'pkg-config --libs sfml-all' along with your g++ command
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-*/
+ */
 
 /* 
  * File:   main.cpp
@@ -25,26 +25,55 @@
 #include <SFML/Graphics.hpp>
 
 using namespace std;
+using namespace sf;
 
 /*
  * Timber : Replica of Timberman Steam Game
  */
 int main(int argc, char** argv) {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // Create a video mode object
+    VideoMode vm(1920, 1080);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+    // Create and open a window for the game
+    RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+    // Create a texture to hold a graphic on the GPU
+    Texture textureBackground;
+
+    // Load a graphic into the texture
+    textureBackground.loadFromFile("assets/graphics/background.png");
+
+    // Create a sprite
+    Sprite spriteBackground;
+
+    // Attach the texture to the sprite
+    spriteBackground.setTexture(textureBackground);
+
+    // Set the sprite to cover the screen
+    spriteBackground.setPosition(0, 0);
+
+    while (window.isOpen()) {
+        /*
+            Handle the player input
+         */
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            window.close();
         }
 
+        /*
+            Update the scene
+         */
+
+        /*
+            Draw the scene
+         */
+        // Clear everything from last frame
         window.clear();
-        window.draw(shape);
+
+        // Draw our game scene here
+        window.draw(spriteBackground);
+
+        // Show everything we just drew
         window.display();
     }
 
